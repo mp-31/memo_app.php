78b81e89d6aa53ea69a2ddf62f2b597051fbe9f5
mp <107902074+mp-31@users.noreply.github.com>
2022-09-24 15:38:17 +0900

    Add files via upload

--- memo_app.php
+++ memo_app.php
@@ -1,6 +1,21 @@
 <?php
 
-function addMemo()
+// DBと接続する関数。接続情報は$link
+function dbConnect()
+{
+  $link = mysqli_connect('db', 'book_log', 'pass', 'book_log');
+
+  if (!$link) {
+    echo 'Error:データベースに接続できませんでした' . PHP_EOL;
+    echo 'Debugging error:' . mysqli_connect_error() . PHP_EOL;
+    exit;
+  }
+  echo 'データベースに接続できました' . PHP_EOL;
+  return $link;
+}
+
+// メモをDBに登録する関数。うまくいっている
+function addMemo($link)
 {
   echo 'メモを登録してください' . PHP_EOL . PHP_EOL;
   echo 'タイトル:';
@@ -12,18 +27,32 @@ function addMemo()
   echo '内容:';
   $memo = trim(fgets(STDIN));
 
-  echo 'メモを登録しました' . PHP_EOL . PHP_EOL;
+  $sql = <<<EOT
+  INSERT INTO memo_app (
+    title,
+    date,
+    memo
+  ) VALUES (
+    "{$title}",
+    $date,
+    "{$memo}"
+  )
+  EOT;
 
-  return [
-    'title' => $title,
-    'date' => $date,
-    'memo' => $memo,
-  ];
+    $result = mysqli_query($link, $sql);
+    if ($result) {
+      echo 'データを追加しました' . PHP_EOL;
+    } else {
+      echo 'Error: データの追加に失敗しました' . PHP_EOL;
+      echo 'Debugging error;' . mysqli_error($link) . PHP_EOL;
+    }
+
+
+  echo 'メモを登録しました' . PHP_EOL . PHP_EOL;
 
 }
 
-$memoDate = [];
-
+// メモをDBから情報を引っ張ってきて、表示する関数。今のところうまくいっていない。
 function listMemo($memoDate)
 {
   echo 'メモを表示します' . PHP_EOL . PHP_EOL;
@@ -35,7 +64,10 @@ function listMemo($memoDate)
   }
 }
 
+$memoDate = [];
+$link = dbConnect();
 
+// メモのメニュー。上記で定義した関数をメニューごとに実行している
 while(true) {
   echo 'メモ帳アプリへようこそ！' . PHP_EOL . PHP_EOL;
   echo '1.メモを登録' . PHP_EOL;
@@ -45,10 +77,12 @@ while(true) {
   $num = trim(fgets(STDIN));
 
   if ($num === '1') {
-    $memoDate[] = addMemo();
+    $memoDate[] = addMemo($link);
   } elseif ($num === '2'){
     listMemo($memoDate);
   } elseif ($num === '9'){
+    mysqli_close($link);
+    echo 'データベースとの接続を切断しました' . PHP_EOL;
     break;
   }
 }
